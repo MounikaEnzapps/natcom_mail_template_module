8cd5e0dee13a75978a122863848d06a0824c4639
USER <yarmiztechnlogies@gmail.com>
2022-01-10 19:59:17 +0530

    <MounikaEnzapps>

---
+++ models/account_move.py
@@ -0,0 +1,15 @@
+from odoo import fields,models,api
+import convert_numbers
+# from deep_translator import GoogleTranslator
+from uuid import uuid4
+import qrcode
+from odoo.exceptions import UserError
+
+import base64
+import logging
+
+from lxml import etree
+
+logger = logging.getLogger(__name__)
+
+
